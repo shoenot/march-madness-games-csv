381e311d04ed8fce34ffde42407713d46db4bc4b
shoenot <mail@shurjo.me>
2025-03-20 01:43:19 +0600

    added combined csv

--- generate_data.py
+++ generate_data.py
@@ -5,6 +5,7 @@ from extract_wiki_from_html import extract_wiki_from_html
 from extract_json_from_wiki import extract_json_from_wiki
 from extract_csv_from_json import extract_csv_from_json
 import re
+import csv
 
 root_dir = Path(__file__).parent
 
@@ -31,5 +32,26 @@ def generate_data():
         extract_json_from_wiki(wiki_path, json_path)
         extract_csv_from_json(json_path, csv_path)
 
+def generate_combined_csv():
+    data = []
+    for csv_path in sorted(Path(root_dir / "csv").iterdir(), key=str, reverse=True):
+        if csv_path.name == "combined.csv":
+            continue
+        with open(csv_path, 'r', newline='') as fp:
+            reader = csv.reader(fp)
+            # skip header 
+            next(reader)
+            for row in reader:
+                data.append(row)
+    combined_csv_path = root_dir / "csv" / "combined.csv"
+    with open(combined_csv_path, 'w') as fp:
+        fieldnames = ["year", "round_of", 
+                      "winning_team_name", "winning_team_seed", "winning_team_score", 
+                      "losing_team_name", "losing_team_seed", "losing_team_score"]
+        writer = csv.writer(fp)
+        writer.writerow(fieldnames)
+        writer.writerows(data)
+
 if __name__ == "__main__":
     generate_data()
+    generate_combined_csv()
